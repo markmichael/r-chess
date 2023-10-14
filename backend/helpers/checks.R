4cd1ad3671a35435bd3dc1a9267258c6db7339b4
Mark Michael <markmichael14@gmail.com>
2023-10-14 09:04:51 -0600

    WIP: add castling

--- backend/helpers/checks.R
+++ backend/helpers/checks.R
@@ -1,7 +1,7 @@
 check_for_checks <- function(game, color) {
   ### get position of king
   king_position <- get_king_position(game, color)
-if (is_king_in_check(game, king_position)) {
+if (is_position_attacked(game@board, king_position)) {
     game@check=TRUE
 }
 ### check available moves for kings position
@@ -12,16 +12,16 @@ get_king_position <- function(game, color) {
     lapply(column, function(row) {
       if (game@board[[column]][[row]]@color == color &&
         game@board[[column]][[row]]@piece_type == "king") {
-        return(list(column, row))
+        return(list(col = column, row = row))
       }
     })
   })
 }
 
-is_king_in_check <- function(game, king_position) {
-    lapply(game@board, function(column) {
+is_position_attacked <- function(board, test_square) {
+    lapply(board, function(column) {
         any(lapply(column, function(row) {
-            return(any(row@available_moves == king_position))
+            return(any(row@available_moves == test_square))
         }))
     })
 }
--- backend/helpers/king.R
+++ backend/helpers/king.R
@@ -1,22 +1,43 @@
 king_available_moves <- function(board, piece) {
-  if (piece@piece_type != "king") {
+    if (piece@piece_type != "king") {
+        return(piece)
+    }
+    piece <- queen_available_moves(board, piece)
     return(piece)
-  }
-  piece <- queen_available_moves(board, piece)
-  return(piece)
 
-  piece@available_moves <- piece@available_moves |>
-    lapply(function(x) {
-      if ((x$col %in% c(
-        letters[which(letters == piece@col) + 1],
-        letters[which(letters == piece@col) - 1],
-        letters[which(letters == piece@col)]
-      )) &&
-        x$row %in% c(piece@row - 1, piece@row, piece@row + 1)) {
-        return(x)
-      } else {
-        return(NULL)
-      }
-    })
-  return(piece)
+    piece@available_moves <- piece@available_moves |>
+        lapply(function(x) {
+            if ((x$col %in% c(
+                letters[which(letters == piece@col) + 1],
+                letters[which(letters == piece@col) - 1],
+                letters[which(letters == piece@col)]
+            )) &&
+                x$row %in% c(piece@row - 1, piece@row, piece@row + 1)) {
+                return(x)
+            } else {
+                return(NULL)
+            }
+        })
+    ### check for short castle
+    if (piece@moved == FALSE &&
+        piece@row == 1 &&
+        board[[letters[which(letters == piece@col) + 1]]][[piece@row]]@color == "none" &&
+        board[[letters[which(letters == piece@col) + 2]]][[piece@row]]@color == "none" &&
+        board[[letters[which(letters == piece@col) + 3]]][[piece@row]]@moved == FALSE &&
+        is_position_attacked(
+            board,
+            list(col = letters[which(letters == piece@col) + 1], row = piece@row)
+        ) &&
+        is_position_attacked(
+            board,
+            list(col = letters[which(letters == piece@col) + 2], row = piece@row)
+        ) &&
+        is_position_attacked(
+            board,
+            list(col = letters[which(letters == piece@col) + 3], row = piece@row)
+        )) {
+        piece@available_moves <- piece@available_moves |>
+            append(list(list(col = piece@col, row = piece@row + 2)))
+    }
+    return(piece)
 }
